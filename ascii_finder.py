1fef5842f52896e294bf7f1294708ac78fde0d33
Bilge Gocer <gocer.bilge91@gmail.com>
2021-02-27 23:01:33 +0000

    ASCII finder

---
+++ ascii_finder.py
@@ -0,0 +1,9 @@
+# Program to find the character from an input ASCII value
+
+# getting ASCII value from user
+num = int(input("Enter ASCII value: "))
+print(chr(num))
+
+# ASCII value is given
+num2 = 70
+print(chr(num2))
